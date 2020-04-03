56b288deb107ad1beb34418ec1c365596c77fddf
androidtest201912 <androidtest201912@test.com>
2020-04-03 23:56:04 +0900

    Commit.

--- make.bat
+++ make.bat
@@ -1,4 +1,3 @@
-vbc /target:winexe  /out:VBTextEditor.exe /main:frmMain  frmMain.vb Application.Designer.vb AssemblyInfo.vb frmAbout.Designer.vb frmAbout.vb frmGotoLine.Designer.vb frmGotoLine.vb frmMain.Designer.vb  frmReplace.Designer.vb frmReplace.vb Resources.Designer.vb Settings.Designer.vb  /reference:ICSharpCode.TextEditor.dll
-rem vbc /target:winexe /out:VBTextEditor.exe /main:frmMain  frmMain.vb Application.Designer.vb AssemblyInfo.vb frmAbout.Designer.vb frmAbout.vb frmGotoLine.Designer.vb frmGotoLine.vb frmMain.Designer.vb  frmReplace.Designer.vb frmReplace.vb  Settings.Designer.vb  /reference:ICSharpCode.TextEditor.dll
+vbc /target:winexe  /out:VBTextEditor.exe /main:frmMain  frmMain.vb Application.Designer.vb AssemblyInfo.vb frmAbout.Designer.vb frmAbout.vb frmGotoLine.Designer.vb frmGotoLine.vb frmMain.Designer.vb  frmReplace.Designer.vb frmReplace.vb Resources.Designer.vb Settings.Designer.vb
 VBTextEditor.exe
 pause
